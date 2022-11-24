8b6c8fff91a84e64bbcc3ca38abd124f1f57be1e
ChaoticShadow <6554395+ChaoticShadow@users.noreply.github.com>
2022-11-23 18:08:32 -0600

    added repo link, reorg files

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import GameCanvas from "./canvas.js";
 import GameInfo, { GuessStatus } from "./info.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  document.getElementById("version").innerText = "v0.0.13";
+  document.getElementById("version").innerText = "v0.1.0";
 
   const game = new Game();
   const gameCanvas = new GameCanvas(document.getElementById("canvas"));
